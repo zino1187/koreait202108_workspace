2b05e395bf218e7603391ee2f9d868a556d7c829
zino1187 <zino1187@gmail.com>
2021-09-01 14:38:46 +0900

    c

--- BookApp/src/com/koreait/bookapp/view/LoginForm.java
+++ BookApp/src/com/koreait/bookapp/view/LoginForm.java
@@ -10,13 +10,16 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
+import com.koreait.bookapp.model.Admin;
+import com.koreait.bookapp.model.AdminDAO;
+
 //도서관리 페이지
 public class LoginForm extends Page{
 	JTextField t_id;
 	JPasswordField t_pass;
 	JButton bt_login;
 	JPanel content;
-	
+	AdminDAO adminDAO;
 	
 	public LoginForm(MainFrame mainFrame) {
 		super(mainFrame);
@@ -26,6 +29,7 @@ public class LoginForm extends Page{
 		t_pass  = new JPasswordField();
 		bt_login = new JButton("Login");
 		content = new JPanel();
+		adminDAO = new AdminDAO();
 		
 		//스타일
 		Dimension d = new Dimension(480,40);
@@ -45,6 +49,16 @@ public class LoginForm extends Page{
 		bt_login.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
+				//아이디와 패스워드를 VO담아서 전달!! 
+				String id=t_id.getText(); //암시적 생성법 
+				String pass=new String(t_pass.getPassword());   // 'a' 'p' 'p' 'l' 'e'  --> "apple"
+				
+				//VO를 생성하여 아이디와 패스워드를 심어서 보내자!!
+				Admin admin = new Admin();//empty
+				admin.setId(id);
+				admin.setPassword(pass);
+				
+				adminDAO.select(admin);
 			}
 		});
 		
